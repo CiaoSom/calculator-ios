77ae6658485a774a9cc99cea70545d229771f62e
calvinCar <m17610602708@163.com>
2018-10-24 15:24:09 +0800

    Initial Commit

---
+++ calculator-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  calculator-ios
+//
+//  Created by jinsong5 on 2018/10/24.
+//  Copyright © 2018年 jinsong5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
